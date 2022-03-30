75052ed6da856e1749c0be7c2812c171f6eb50bd
xxrz <xxurongze@gmail.com>
2022-03-30 23:51:43 +0800

    come on

--- codes/src/Solution.java
+++ codes/src/Solution.java
@@ -1,22 +1,19 @@
 import java.util.*;
 
 class Solution {
-    public int singleNumber(int[] nums) {
-        int[] count = new int[32];
-        //统计好每一位为1的数量
-        for(int num:nums){
-            for(int i=0;i<32;i++){
-                count[i] += num&1;
-                //num右移
-                num >>>=1; //此时在最低位,为了判断下一个高位，需要右移
-            }
+    public int maxValue(int[][] grid) {
+
+        return dfs(grid,0,0);
+    }
+
+    public int dfs(int[][] grid,int i,int j){
+        if(i == grid.length || j == grid[0].length){
+            return 0;
         }
 
-        int res = 0,m=3;
-        for(int i= 0;i<32;i++){
-            res <<= 1;
-            res |= count[31-i] % m;
-        }
-        return res;
+        int r = dfs(grid,i,j+1);
+        int b = dfs(grid,i+1,j);
+
+        return Math.max(r,b) + grid[i][j];
     }
 }

45ed0875e603a3ea2137e9c466e356531cf76198
xxrz <xxurongze@gmail.com>
2022-04-06 16:14:18 +0800

    come on

--- codes/src/Solution.java
+++ codes/src/Solution.java
@@ -1,52 +1,40 @@
 import java.util.*;
 
 class Solution {
-     //省空间的做法，更改当前已访问过的元素，在结束此次搜索后还原
-     public boolean exist(char[][] board, String word) {
+    public int lengthOfLongestSubstring(String s) {
+        //思路：
+        //1，左闭右开，hashmap中放的是索引值而不是单纯的次数
+        //2，扩大right,并更新windows中字符的索引值
+        //3，left缩小窗口的时候，要注意不是单纯的left++，要判断当前left的位置和这个字符上一次出现的索引+1的大小
+        //4，结果在缩小窗口之后
 
-         for(int i = 0;i < board.length;i++){
-             for(int j=0;j < board[0].length;j++){
-                 boolean res = dfs(board,i,j,word,0);
-                 if(res){
-                     return true;
-                 }
-             }
-         }
-         return false;
-     }
+        //定义变量
+        HashMap<Character,Integer> windows = new HashMap<>(); //记录的是上个字符的索引值
+        int left = 0, right = 0; //滑动窗口，左闭右开
+        int maxlen = Integer.MIN_VALUE;
 
-     //二维数组，以当前（i,j）节点为头开始递归，word所求词，idx为遍历到word的第几个词,
-     public boolean dfs(char[][]board, int i, int j, String word,int idx){
-         //如果当前字符也不相等，则直接返回（剪枝）+ 边界
-         //边界条件没有想清楚
-         if(i < 0 || j < 0 || i == board.length || j==board[0].length || board[i][j]!=word.charAt(idx)){
-             return false;
-         }
+        //滑动窗口
+        while(right < s.length()){
+            //当前移入窗口的字符
+            char c = s.charAt(right);
+            System.out.println("left:"+left+"\tright:"+right + "\t:" + s.substring(left,right));
 
-         //排除遍历过的节点
-         if(board[i][j]=='\0') return false;
+            //右移窗口
+            right++;
 
-         if(idx == word.length()-1 ) return true;
+            //判断窗口是否要收缩
+            if(windows.containsKey(c)){
+                //左移窗口
+                System.out.println("\tleft:"+left+"\twindows:"+(int)(windows.get(c)+1));
+                left = windows.get(c)+1;   //这块一定要取更大的那个，而不是单纯的left++
+//                left = Math.max(left,windows.get(c)+1);   //这块一定要取更大的那个，而不是单纯的left++
+            }
+            //更新结果
+            System.out.println("\t\tleft:"+left + "\tright:"+right);
+            maxlen = Math.max(right-left, maxlen);
+            windows.put(c, right-1);
 
-         //将当前节点标记为已访问
-         char tmp = board[i][j];
-         board[i][j]='\0';
-
-         //定义上下左右四个方向
-         int[] di ={0,0,-1,1};
-         int[] dj = {1,-1,0,0};
-
-         //开使递归
-         //只是针对此次搜索的res
-         boolean res = false;
-         for(int k = 0; k<4 ;k++){
-             res |= dfs(board,i+di[k],j+dj[k],word,idx+1);
-         }
-
-         //回溯
-         // 因为只代表此次搜索过程中，该元素已访问过，当初始i j变化时，又开始了另一次搜索过程
-         board[i][j] = tmp;
-         return res;
-
-     }
+        }
+        return maxlen==Integer.MIN_VALUE?0:maxlen;
+    }
 }

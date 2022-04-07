d000e77522644d9e40562e06b4c305a48be0d16e
xxrz <xxurongze@gmail.com>
2022-04-07 13:57:05 +0800

    hello world

--- codes/src/Solution.java
+++ codes/src/Solution.java
@@ -1,40 +1,31 @@
 import java.util.*;
 
 class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        //思路：
-        //1，左闭右开，hashmap中放的是索引值而不是单纯的次数
-        //2，扩大right,并更新windows中字符的索引值
-        //3，left缩小窗口的时候，要注意不是单纯的left++，要判断当前left的位置和这个字符上一次出现的索引+1的大小
-        //4，结果在缩小窗口之后
+    public int[][] findContinuousSequence(int target) {
+        List<int[]> res = new ArrayList<>();
+        int left = 1,right = 1;
+        int sum = 0;
 
-        //定义变量
-        HashMap<Character,Integer> windows = new HashMap<>(); //记录的是上个字符的索引值
-        int left = 0, right = 0; //滑动窗口，左闭右开
-        int maxlen = Integer.MIN_VALUE;
-
-        //滑动窗口
-        while(right < s.length()){
-            //当前移入窗口的字符
-            char c = s.charAt(right);
-            System.out.println("left:"+left+"\tright:"+right + "\t:" + s.substring(left,right));
-
-            //右移窗口
+        while(left <= target/2){
+            int c = right;
             right++;
 
-            //判断窗口是否要收缩
-            if(windows.containsKey(c)){
-                //左移窗口
-                System.out.println("\tleft:"+left+"\twindows:"+(int)(windows.get(c)+1));
-                left = windows.get(c)+1;   //这块一定要取更大的那个，而不是单纯的left++
-//                left = Math.max(left,windows.get(c)+1);   //这块一定要取更大的那个，而不是单纯的left++
-            }
-            //更新结果
-            System.out.println("\t\tleft:"+left + "\tright:"+right);
-            maxlen = Math.max(right-left, maxlen);
-            windows.put(c, right-1);
+            sum+=c;
 
+            while(sum >= target){
+                if(sum==target){
+                    int[] ans = new int[c-left+1];
+                    for(int k = left;k <= c;k++){
+                        ans[k-left] = k;
+                    }
+                    res.add(ans);
+                }
+                int d = left;
+                left++;
+                sum = sum-d;
+            }
         }
-        return maxlen==Integer.MIN_VALUE?0:maxlen;
+
+        return res.toArray(new int[0][]);
     }
 }
--- codes/src/testMain.java
+++ codes/src/testMain.java
@@ -24,8 +24,12 @@ public class testMain {
 //                {7,1,9,6,1,2,7,0,9,6,6,4,4,5,1,0},
 //                {3,4,9,2,8,3,1,2,6,9,7,0,2,4,2,0},
 //                {5,1,8,8,4,6,8,5,2,4,1,6,2,2,9,7}};
-        int res = test.lengthOfLongestSubstring(s);
-        System.out.println(res);
+        int tar = 9;
+        int[][] res = test.findContinuousSequence(tar);
+        for(int i = 0;i<res.length;i++){
+            System.out.println(Arrays.toString(res[i]));
+        }
+//        System.out.println(res);
 //        System.out.println(Arrays.toString(res));
     }
 }

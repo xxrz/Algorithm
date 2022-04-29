1ad20bc5559f79528126b2686b50ee2a49f88dda
xxrz <xxurongze@gmail.com>
2022-04-29 15:07:58 +0800

    dp

--- codes/src/Solution.java
+++ codes/src/Solution.java
@@ -1,18 +1,61 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 class Solution {
-    public int[] dailyTemperatures(int[] temperatures) {
-        int[] res = new int[temperatures.length];
-        Stack<Integer> s = new Stack<>();
-
-        for(int i = temperatures.length-1;i>=0;i--){
-            while(!s.empty() && temperatures[s.peek()] <= temperatures[i]){
-                s.pop();
-            }
-            res[i] = s.empty()? 0: s.peek()-i;
-            s.add(i);
+    //转成完全背包问题
+    //状态：所选的数，价值
+    //dp[i][j]：可以选择0-i-1的数的平方，现在和为j的最小的个数
+    //状态转移：当前的数的平方选还是不选
+    //  dp[i][j] = Math.min(dp[i-1][j],dp[i][j-i^2] + 1)
+    //初始值：
+    //  dp[0][0]=0,其他都是最大值
+    //结果：
+    //  dp[m][n]
+    public int numSquares(int n) {
+        List<Integer> list = new ArrayList<>();
+        int t=1;
+        while(t*t<=n){
+            list.add(t*t);
+            t++;
         }
 
-        return res;
+        int m = list.size();
+        int[][] dp = new int[m+1][n+1];
+
+        for(int i = 0;i <= m;i++){
+            Arrays.fill(dp[i],Integer.MAX_VALUE/2);
+        }
+
+        for(int i=1;i<=m;i++){
+            dp[i][0] = 0;
+        }
+
+//        for(int i =0;i<=m;i++){
+//            System.out.println(Arrays.toString(dp[i]));
+//        }
+
+        // dp[0][0] = 0;
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(j-list.get(i-1) < 0){
+                    dp[i][j] = dp[i-1][j];
+                }else{
+                    dp[i][j] = Math.min(dp[i-1][j],(int)(dp[i][j-list.get(i-1)])+1);
+
+//                    System.out.println("dp[i-1][j]: "+(i-1)+","+ j +"  :" + dp[i-1][j]);
+//                    System.out.println("dp[i][j-list.get(i-1)]: "+i+", "+(j-list.get(i-1))+ "  :"+ (int)(dp[i][j-list.get(i-1)])+1);
+//                    System.out.println("dp[i][j]: "+(i)+","+ j +"  :" + dp[i][j]);
+//                    System.out.println();
+                }
+            }
+        }
+
+//        System.out.println();
+//        for(int i =0;i<=m;i++){
+//            System.out.println(Arrays.toString(dp[i]));
+//        }
+
+        return dp[m][n];
     }
 }
--- codes/src/testMain.java
+++ codes/src/testMain.java
@@ -4,8 +4,8 @@ public class testMain {
     public static void main(String[] args) {
 //        int[] arr = {9,1,7,9,7,9,7};
         Solution test = new Solution();
-        String s = "abba";
-        String t = "ab";
+//        String s = "abba";
+//        String t = "ab";
 //        int[][] arr = {{3,8,6,0,5,9,9,6,3,4,0,5,7,3,9,3},
 //                {0,9,2,5,5,4,9,1,4,6,9,5,6,7,3,2},
 //                {8,2,2,3,3,3,1,6,9,1,1,6,6,2,1,9},
@@ -24,14 +24,15 @@ public class testMain {
 //                {7,1,9,6,1,2,7,0,9,6,6,4,4,5,1,0},
 //                {3,4,9,2,8,3,1,2,6,9,7,0,2,4,2,0},
 //                {5,1,8,8,4,6,8,5,2,4,1,6,2,2,9,7}};
-        int tar = 9;
-        int[] data  = {73,74,75,71,69,72,76,73};
+//        int tar = 9;
+//        int[] data  = {73,74,75,71,69,72,76,73};
 //        int[] data  = {30,40,20,50};
-        int[] res = test.dailyTemperatures(data);
+//        int[] res = test.dailyTemperatures(data);
 //        for(int i = 0;i<res.length;i++){
 //            System.out.println(Arrays.toString(res[i]));
 //        }
 //        System.out.println(res);
-        System.out.println(Arrays.toString(res));
+//        System.out.println(Arrays.toString(res));
+        int res  = test.numSquares(13);
     }
 }
